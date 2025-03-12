eb907efedb6635170abda9a5c8074e64f017c97b
Jaime Lee <18038163520@163.com>
2025-03-13 01:33:30 +0800

    Update likkim.mk

--- likkim_core/likkim/likkim.mk
+++ likkim_core/likkim/likkim.mk
@@ -1,6 +1,11 @@
-﻿LIKKIM_DIR_NAME ?= likkim
+﻿# Set LIKKIM_DIR_NAME to 'likkim' if not set
+LIKKIM_DIR_NAME ?= likkim
 
+# Include the src.mk file for compiling code
 include $(LVGL_DIR)/$(LIKKIM_DIR_NAME)/src/src.mk
-include $(LVGL_DIR)/$(LIKKIM_DIR_NAME)/img/img.mk
-include $(LVGL_DIR)/$(LIKKIM_DIR_NAME)/font/font.mk
 
+# Include the img.mk file for handling images
+include $(LVGL_DIR)/$(LIKKIM_DIR_NAME)/img/img.mk
+
+# Include the font.mk file for handling fonts
+include $(LVGL_DIR)/$(LIKKIM_DIR_NAME)/font/font.mk
